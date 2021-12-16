c1746dca16bc0508ff3ae86ac4fe6077a64a30b9
pineda.juan92 <pineda.juan92@gmail.com>
2021-12-15 21:41:05 -0500

    checked state and handleText function created to show and hide p

--- src/App.js
+++ src/App.js
@@ -13,3 +13,50 @@ class App extends Component {
 }
 
 export default App;
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.handleText = this.handleText.bind(this);
+    this.state = {
+      checked: false,
+    };
+  }
+
+  handleText() {
+    this.setState({
+      checked: !this.state.checked,
+    });
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <label>
+          <input
+            type="checkbox"
+            onChange={this.handleText}
+          />{" "}
+          Mostrar información importante
+        </label>
+        {this.state.checked ? (
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        ) : (
+          ""
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
